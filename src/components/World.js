6c5b41e1e8c62908de1bfb09c78f1b7c234e233d
Jared Williams <jareddw@berkeley.edu>
2022-11-28 17:50:37 -0800

    got world image to load in homepage.

--- src/components/World.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const World = () => {
-  return (
-    <img src="../assets/world.png" />
-  )
-}
-
-export default World
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Waves from '../components/Waves'
 import "./Home.css"
-import World from '../components/World'
 import Footer from '../components/Footer'
+import WorldImage from '../assets/world2.png'
 
 const Home = () => {
   return (
@@ -14,7 +14,7 @@ const Home = () => {
     <div className="homeBody">
         <h1 className="header"> Raising the Standard of Sustainable Solutions With</h1>
         <h1 className="header2"> <span className="changingText">Fair Labor Practices</span> </h1>
-        <img src={require("../assets/world.png")} />
+        <img src={WorldImage} />
         <p>A group of university researchers committed to addressing sustainability and
             climate change from the root in order to inform holistic solutions</p>
     </div>
