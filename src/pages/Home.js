49bfa43f5f92cbad3e949e6c0a398711a0d50d5b
Jared Williams <jareddw@berkeley.edu>
2022-11-28 18:34:35 -0800

    added typewriter effect! but only one option for text rn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import Waves from '../components/Waves'
 import "./Home.css"
 import Footer from '../components/Footer'
 import WorldImage from '../assets/world2.png'
+import Typewriter from 'typewriter-effect/dist/core';
 
 const Home = () => {
+  useEffect(()=> {
+    var app = document.getElementById('changingText');
+    var customNodeCreator = function(character) {
+      return document.createTextNode(character);
+    }
+  
+    var typewriter = new Typewriter(app, {
+      loop: true,
+      delay: 75,
+      onCreateTextNode: customNodeCreator,
+    });
+  
+    typewriter
+      .typeString('Social impact')
+      .pauseFor(300)
+      .start();
+  }, [])
+
   return (
     <>
     
@@ -13,7 +32,7 @@ const Home = () => {
     
     <div className="homeBody">
         <h1 className="header"> Raising the Standard of Sustainable Solutions With</h1>
-        <h1 className="header2"> <span className="changingText">Fair Labor Practices</span> </h1>
+        <h1 className="header2"> <span id="changingText">Fair Labor Practices</span> </h1>
         <img src={WorldImage} />
         <p>A group of university researchers committed to addressing sustainability and
             climate change from the root in order to inform holistic solutions</p>
