cbd310f5a3da9644bfaefa6e614a492cf8410634
“Sean <sean.she@berkeley.edu>
2023-01-25 19:55:04 -0800

    final push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,13 @@ const Footer = () => {
         <div className = 'FooterWrap'>
             <div className = 'FooterContainer'>
                 <div className = 'footer-item'>Berkeley, CA</div>
-                </div>
+            </div>
             <div className = 'FooterContainer'>
                 {/* Change to a tag and add email later */}
                 <div className = 'footer-item'>Email</div>
-                </div>
+            </div>
             <div className = 'FooterContainer'>
                 <div className = 'footer-item'>SURG</div>
-                
             </div>
         </div>
     )
